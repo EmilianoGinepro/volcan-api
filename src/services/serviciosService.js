2c82916b8b2ff5937ff77900bc572a71ccadb644
emiliano <emilianototoginepro@gmail.com>
2022-10-30 18:00:22 -0300

    mejorado el get

--- src/services/serviciosService.js
+++ src/services/serviciosService.js
@@ -2,10 +2,12 @@ const { db } = require('../config/dbConfig')
 
 const getServicios = async (req, res) => {
     try {
-           const serv = await db.ref('servicio').once('value', (snapshot) => {
+          /*  const serv = await db.ref('servicio').once('value', (snapshot) => {
             snapshot.val();
-        });
-        return serv
+        }); */
+        const servicioRef = db.ref('servicio')
+        const data = await servicioRef.get()
+        return data
     } catch (err) {
         return res.status(404).send(err);
     };
--- src/v1/routes/routes.js
+++ src/v1/routes/routes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const apiCache = require('apicache');
 const emailCotroller = require('../../controllers/emailController');
 const servicioController = require('../../controllers/serviciosControllers')
 
+const cache = apiCache.middleware
 
 router
     .get('/', function (req, res) {
         res.send('Volcan Prodcutora API')
     })
     .post('/', emailCotroller.postSendEmail)
-    .get('/servicio/get', servicioController.getServicios)
+    .get('/servicio/get', cache("1 minutes"), servicioController.getServicios)
 
 module.exports = router;
