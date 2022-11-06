1cbdd9f2f1fe0fd4c55db1329e5f9c55b883eceb
emiliano <emilianototoginepro@gmail.com>
2022-11-06 14:13:23 -0300

    mejoras en el envio de email

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -3,7 +3,6 @@ const emailService = require('../services/emailService');
 const postSendEmail = async (req, res) => {
     const { body } = req;
     const { nombre, empresa, email, opcion, textarea } = body;
-
     try {
         const sendEmail = await emailService.postSendEmail(nombre, empresa, email, opcion, textarea, res);
         res.status(202).json(sendEmail)
